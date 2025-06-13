981def9ddad3e70b83a79c01da90b0edf0b2b623
ADH-17 <ayden.humphries17@gmail.com>
2025-06-13 16:05:31 -0700

    added practice for variables

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log('Hello World'); // strings kept in ' ' 
+let name = 'Ayden';
+console.log(name);
+
+// variables and concatination
+let firstName = 'Ayden';
+let lastName = 'Humphries';
+name = firstName + ' ' + lastName;
+console.log(name);
+let age = 20;
+let isApproved = true; 
+
+// const practice
+const interestRate = 0.3;
+//interestRate = 1; should cause error
+console.log(interestRate);
